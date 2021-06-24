f5d458a9566ed24c2aec32f9307fc42fa93c00ea
Olivier Lavoie <olivier.lavoie@nosotech.com>
2021-06-24 10:22:16 -0400

    Added "#!run notbook <path>" command

--- TAO3/Internal/Commands/Run/RunCommand.cs
+++ TAO3/Internal/Commands/Run/RunCommand.cs
@@ -1,13 +1,17 @@
 ﻿using Microsoft.DotNet.Interactive;
 using Microsoft.DotNet.Interactive.Commands;
+using Microsoft.DotNet.Interactive.Notebook;
 using System;
 using System.Collections.Generic;
 using System.CommandLine;
 using System.CommandLine.Invocation;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using TAO3.Cell;
+using NotebookCell = Microsoft.DotNet.Interactive.Notebook.NotebookCell;
+using TAO3NotebookCell = TAO3.Cell.NotebookCell;
 
 namespace TAO3.Internal.Commands.Run
 {
@@ -29,7 +33,7 @@ namespace TAO3.Internal.Commands.Run
             {
                 if (evnt is CellAddedEvent cellAddedEvent)
                 {
-                    NotebookCell cell = cellAddedEvent.Cell;
+                    TAO3NotebookCell cell = cellAddedEvent.Cell;
                     Command runCellCommand = new Command(cell.Name);
 
                     runCellCommand.Handler = CommandHandler.Create(async (KernelInvocationContext context) =>
@@ -51,7 +55,27 @@ namespace TAO3.Internal.Commands.Run
 
         private Command CreateRunNotebookCommand()
         {
-            return new Command("notebook");
+            Command command = new Command("notebook")
+            {
+                new Argument<string>("path", "Path to a .dib or .ipynb file")
+            };
+
+            command.Handler = CommandHandler.Create(async (string path, KernelInvocationContext context) =>
+            {
+                if (!File.Exists(path))
+                {
+                    throw new FileNotFoundException("File not found", path);
+                }
+
+                NotebookDocument notebook = Kernel.Current.ParentKernel.ParseNotebook(path, File.ReadAllBytes(path));
+                foreach (NotebookCell cell in notebook.Cells)
+                {
+                    //todo: handle errors
+                    await Kernel.Current.ParentKernel.SendAsync(new SubmitCode(cell.Contents, cell.Language));
+                }
+            });
+
+            return command;
         }
     }
 }
