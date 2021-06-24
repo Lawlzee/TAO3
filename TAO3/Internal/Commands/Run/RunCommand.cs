a4d075c71227ea01945f21cbe0e5eaf1a843a1c1
Olivier Lavoie <olivier.lavoie@nosotech.com>
2021-06-24 12:24:53 -0400

    Added autocomplet for path in "#!run notebook <path>"

--- TAO3/Internal/Commands/Run/RunCommand.cs
+++ TAO3/Internal/Commands/Run/RunCommand.cs
@@ -5,6 +5,7 @@ using System;
 using System.Collections.Generic;
 using System.CommandLine;
 using System.CommandLine.Invocation;
+using System.CommandLine.Parsing;
 using System.IO;
 using System.Linq;
 using System.Text;
@@ -50,14 +51,19 @@ namespace TAO3.Internal.Commands.Run
 
         private Command CreateRunVariableCommand()
         {
-            return new Command("variable");
+            Command command = new Command("variable")
+            {
+                new Argument<string>("name", "name of the variable")
+            };
+
+            return command;
         }
 
         private Command CreateRunNotebookCommand()
         {
             Command command = new Command("notebook")
             {
-                new Argument<string>("path", "Path to a .dib or .ipynb file")
+                new FilePathArgument()
             };
 
             command.Handler = CommandHandler.Create(async (string path, KernelInvocationContext context) =>
@@ -76,6 +82,100 @@ namespace TAO3.Internal.Commands.Run
             });
 
             return command;
+        }
+
+        private class FilePathArgument : Argument<string>
+        {
+            public FilePathArgument() : base("path", "Path to a .dib or .ipynb file")
+            {
+            }
+
+            public override IEnumerable<string?> GetSuggestions(ParseResult? parseResult = null, string? textToMatch = null)
+            {
+                if (textToMatch == null || parseResult == null)
+                {
+                    return base.GetSuggestions(parseResult, textToMatch);
+                }
+
+                string realTextToMatch = parseResult.CommandResult.Tokens.Count > 0
+                    ? parseResult.CommandResult.Tokens[0].Value
+                    : "";
+
+                if (Path.IsPathFullyQualified(realTextToMatch))
+                {
+                    return GetRootedSuggestions(realTextToMatch);
+                }
+
+                return GetRelativeSuggestion(realTextToMatch);
+
+                IEnumerable<string> GetRootedSuggestions(string textToMatch)
+                {
+                    if (File.Exists(textToMatch))
+                    {
+                        return new[] { AddQuoteIfNecessary(textToMatch) };
+                    }
+
+                    string? currentDirectory = Path.GetDirectoryName(textToMatch);
+                    string fullyQualifiedCurrentDirectory = string.IsNullOrEmpty(currentDirectory)
+                        ? Path.GetPathRoot(textToMatch)!
+                        : currentDirectory!;
+
+                    string? commandDirectory = Path.GetDirectoryName(textToMatch);
+                    string directory = string.IsNullOrEmpty(commandDirectory)
+                        ? fullyQualifiedCurrentDirectory
+                        : commandDirectory;
+
+                    string fileName = Path.GetFileName(textToMatch);
+
+                    return Directory.GetDirectories(directory, fileName + "*")
+                        .Select(x => x + "\\")
+                        .Concat(Directory.GetFiles(directory, fileName + "*"))
+                        .Select(AddQuoteIfNecessary);
+                }
+
+                IEnumerable<string> GetRelativeSuggestion(string textToMatch)
+                {
+                    string currentDirectory = Directory.GetCurrentDirectory();
+                    if (File.Exists(textToMatch))
+                    {
+                        return new[] { AddQuoteIfNecessary(Path.GetRelativePath(currentDirectory, textToMatch)) };
+                    }
+
+                    string? commandDirectory = Path.GetDirectoryName(textToMatch);
+                    string directory = string.IsNullOrEmpty(commandDirectory)
+                        ? currentDirectory
+                        : commandDirectory;
+
+                    string fileName = Path.GetFileName(textToMatch);
+
+                    IEnumerable<string> suggestions = Directory.GetDirectories(directory, fileName + "*")
+                        .Select(x => Path.GetRelativePath(currentDirectory, x) + "\\")
+                        .Concat(Directory.GetFiles(directory, fileName + "*")
+                            .Select(x => Path.GetRelativePath(currentDirectory, x)))
+                        .Select(AddQuoteIfNecessary);
+
+                    if (string.IsNullOrEmpty(commandDirectory))
+                    {
+                        return DriveInfo.GetDrives()
+                            .Select(x => x.Name)
+                            .Where(x => x.StartsWith(textToMatch, StringComparison.OrdinalIgnoreCase))
+                            .Concat(suggestions)
+                            .Select(AddQuoteIfNecessary);
+                    }
+
+                    return suggestions;
+                }
+
+                string AddQuoteIfNecessary(string path)
+                {
+                    if (path.Contains(" "))
+                    {
+                        return "\"" + path + "\"";
+                    }
+
+                    return path;
+                }
+            }
         }
     }
 }
